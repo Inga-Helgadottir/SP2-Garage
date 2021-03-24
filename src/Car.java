dcc0838b1e6eb3fbf55b007d700c5e6ae3e5598a
Inga-Helgadottitr <78418864+Inga-Helgadottitr@users.noreply.github.com>
2021-03-24 11:36:13 +0100

    Create Car.java

---
+++ src/Car.java
@@ -0,0 +1,66 @@
+abstract class Car {
+    private int regNr;
+    private String brand;
+    private String model;
+    private int year;
+    private int nbrOfDoors;
+
+    public Car(int regNr, String brand, String model, int year, int nbrOfDoors) {
+        this.regNr = regNr;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.nbrOfDoors = nbrOfDoors;
+    }
+
+    public int getRegNr() {
+        return regNr;
+    }
+
+    public void setRegNr(int regNr) {
+        this.regNr = regNr;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getNbrOfDoors() {
+        return nbrOfDoors;
+    }
+
+    public void setNbrOfDoors(int nbrOfDoors) {
+        this.nbrOfDoors = nbrOfDoors;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "regNr=" + regNr +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", nbrOfDoors=" + nbrOfDoors +
+                '}';
+    }
+}
