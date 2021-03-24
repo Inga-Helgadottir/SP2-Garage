7b60f3a62d21e05d1a0a23b544d5adcf0eeebee7
Inga-Helgadottitr <78418864+Inga-Helgadottitr@users.noreply.github.com>
2021-03-24 11:36:18 +0100

    Create ElectricCar.java

---
+++ src/ElectricCar.java
@@ -0,0 +1,45 @@
+public class ElectricCar extends Car {
+    private int batteryCapacityKWh;
+    private int maxKm;
+    private int whPrKm;//kmPrL for en el bil
+
+    public ElectricCar(int regNr, String brand, String model, int year, int nbrOfDoors, int batteryCapacityKWh, int maxKm, int whPrKm) {
+        super(regNr, brand, model, year, nbrOfDoors);
+        this.batteryCapacityKWh = batteryCapacityKWh;
+        this.maxKm = maxKm;
+        this.whPrKm = whPrKm;
+    }
+
+    public int getBatteryCapacityKWh() {
+        return batteryCapacityKWh;
+    }
+
+    public void setBatteryCapacityKWh(int batteryCapacityKWh) {
+        this.batteryCapacityKWh = batteryCapacityKWh;
+    }
+
+    public int getMaxKm() {
+        return maxKm;
+    }
+
+    public void setMaxKm(int maxKm) {
+        this.maxKm = maxKm;
+    }
+
+    public int getWhPrKm() {
+        return whPrKm;
+    }
+
+    public void setWhPrKm(int whPrKm) {
+        this.whPrKm = whPrKm;
+    }
+
+    @Override
+    public String toString() {
+        return "ElectricCar{" +
+                "batteryCapacityKWh=" + batteryCapacityKWh +
+                ", maxKm=" + maxKm +
+                ", whPrKm=" + whPrKm +
+                '}';
+    }
+}
